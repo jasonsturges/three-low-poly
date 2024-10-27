728a95ccc9b2443d2a754148f89bc4970a699b08
Jason Sturges <jason@jasonsturges.com>
2024-10-27 00:09:53 -0500

    Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,23 @@
 import dts from "vite-plugin-dts";
 import path from "path";
-import react from "@vitejs/plugin-react";
 import { defineConfig, UserConfig } from "vite";
 
 export default defineConfig({
   base: "./",
-  plugins: [dts({ rollupTypes: true }), react()],
+  plugins: [dts({ rollupTypes: true })],
   build: {
     sourcemap: true,
     lib: {
       entry: path.resolve(__dirname, "src/index.ts"),
-      name: "mylib",
+      name: "ThreeLowPoly",
       formats: ["es", "cjs", "umd", "iife"],
       fileName: (format) => `index.${format}.js`,
     },
     rollupOptions: {
-      external: ["react", "react-dom"],
+      external: ["three"],
       output: {
         globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
+          three: "THREE",
         },
       },
     },
