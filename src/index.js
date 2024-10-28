51d38df22b8c2ada92ca579878019d070538e248
Jason Sturges <jason@jasonsturges.com>
2024-10-27 23:15:00 -0500

    Exports

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
-export { Bone } from "./geometry/Bone";
+//------------------------------
+//  Geometries
+//------------------------------
+
+// Cemetery
+export { CrossHeadstoneGeometry } from "./geometry/cemetery/CrossHeadstoneGeometry";
+export { ObeliskHeadstoneGeometry } from "./geometry/cemetery/ObeliskHeadstoneGeometry";
+export { RoundedHeadstoneGeometry } from "./geometry/cemetery/RoundedHeadstoneGeometry";
+export { SquareHeadstoneGeometry } from "./geometry/cemetery/SquareHeadstoneGeometry";
+
+// Fence
+export { FenceColumn } from "./geometry/fence/FenceColumn";
+export { WroughtIronBarGeometry } from "./geometry/fence/WroughtIronBarGeometry";
+
+// Leafs
+export { SimpleLeafGeometry } from "./geometry/leafs/SimpleLeafGeometry";
+
+// Skeleton
+export { BoneGeometry } from "./geometry/skeleton/BoneGeometry";
+
+//------------------------------
+//  Groups
+//------------------------------
+
+// Astronomy
+export { Moon } from "./group/astronomy/Moon";
+
+// Cemetery
+export { Mausoleum } from "./group/cemetery/Mausoleum";
+
+// Lighting
+export { Candle } from "./group/lighting/Candle";
+export { Lantern } from "./group/lighting/Lantern";
+
+// Rocks
+export { Stones } from "./group/rocks/Stones";
