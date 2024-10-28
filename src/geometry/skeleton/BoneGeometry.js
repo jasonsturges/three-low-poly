d32504c1719e237b31cf35e5a16b698afb932380
Jason Sturges <jason@jasonsturges.com>
2024-10-27 22:10:23 -0500

    Bone geometry

--- src/geometry/skeleton/BoneGeometry.js
+++ src/geometry/skeleton/BoneGeometry.js
@@ -1,27 +1,27 @@
-import * as THREE from "three";
-import * as BufferGeometryUtils from "three/addons/utils/BufferGeometryUtils.js";
+import { BufferGeometry, CylinderGeometry, SphereGeometry } from "three";
+import { mergeGeometries } from "three/addons/utils/BufferGeometryUtils.js";
 
 /**
  * Bone Geometry, a simple bone shape
- * @extends THREE.BufferGeometry
+ * @extends BufferGeometry
  *
  * @example
  * // Create a bone
  * const boneGeometry = new Bone();
- * const boneMaterial = new THREE.MeshStandardMaterial({ color: 0xffffff });
- * const bone = new THREE.Mesh(boneGeometry, boneMaterial);
+ * const boneMaterial = new MeshStandardMaterial({ color: 0xffffff });
+ * const bone = new Mesh(boneGeometry, boneMaterial);
  * scene.add(bone);
  */
-export class Bone extends THREE.BufferGeometry {
+class BoneGeometry extends BufferGeometry {
   constructor(radiusTop = 0.1, radiusBottom = 0.1, height = 0.4, radialSegments = 8) {
     super();
 
     // Create the cylinder (shaft of the bone)
-    const cylinderGeometry = new THREE.CylinderGeometry(radiusTop * 0.6, radiusBottom * 0.6, height, radialSegments);
+    const cylinderGeometry = new CylinderGeometry(radiusTop * 0.6, radiusBottom * 0.6, height, radialSegments);
     cylinderGeometry.translate(0, 0, 0);
 
     // Create the spheres (ends of the bone)
-    const sphereGeometry = new THREE.SphereGeometry(radiusTop, radialSegments, radialSegments);
+    const sphereGeometry = new SphereGeometry(radiusTop, radialSegments, radialSegments);
     const topSphere1 = sphereGeometry.clone();
     const topSphere2 = sphereGeometry.clone();
     const bottomSphere1 = sphereGeometry.clone();
@@ -34,8 +34,8 @@ export class Bone extends THREE.BufferGeometry {
     bottomSphere2.translate(0, -height / 2 - radiusBottom * 0.6, radiusBottom * 0.6);
 
     // Merge the parts
-    this.copy(
-      BufferGeometryUtils.mergeGeometries([cylinderGeometry, topSphere1, topSphere2, bottomSphere1, bottomSphere2], true),
-    );
+    this.copy(mergeGeometries([cylinderGeometry, topSphere1, topSphere2, bottomSphere1, bottomSphere2], true));
   }
 }
+
+export { BoneGeometry };
