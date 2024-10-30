7ad829c6ecc1e172b34cb53375d40dca512f7159
Jason Sturges <jason@jasonsturges.com>
2024-10-30 12:23:45 -0500

    Test Tube

--- src/group/science/TestTube.js
+++ src/group/science/TestTube.js
@@ -1,12 +1,13 @@
-import { CylinderGeometry, DoubleSide, Group, Mesh, MeshPhysicalMaterial, SphereGeometry } from "three";
+import { DoubleSide, Group, Mesh, MeshPhysicalMaterial } from "three";
+import { TestTubeGeometry } from "../../geometry/science/TestTubeGeometry.js";
 
 class TestTube extends Group {
   constructor() {
     super();
 
     // Test Tube Geometry
-    // Create the cylindrical body
-    const tubeGeometry = new CylinderGeometry(0.2, 0.2, 3, 32, 1, true); // Radius top, radius bottom, height
+    const tubeGeometry = new TestTubeGeometry();
+
     const tubeMaterial = new MeshPhysicalMaterial({
       color: 0x88ccff,
       transparent: true,
@@ -17,17 +18,10 @@ class TestTube extends Group {
       transmission: 0.9, // For glass effect
       side: DoubleSide,
     });
+
     const tube = new Mesh(tubeGeometry, tubeMaterial);
 
-    // Create the rounded bottom using a sphere
-    const bottomGeometry = new SphereGeometry(0.2, 32, 16, 0, Math.PI * 2, Math.PI / 2, Math.PI / 2); // Half sphere
-    const bottom = new Mesh(bottomGeometry, tubeMaterial);
-
-    // Position the bottom half-sphere at the base of the tube
-    bottom.position.y = -1.5; // Position it at the bottom of the cylinder
-
     this.add(tube);
-    this.add(bottom);
   }
 }
 
