eda794d8cefa121cf616d101afa010ec20ccbedd
Jason Sturges <jason@jasonsturges.com>
2024-10-30 23:38:39 -0500

    Test Tube geometry

--- src/geometry/science/TestTubeGeometry.js
+++ src/geometry/science/TestTubeGeometry.js
@@ -2,15 +2,15 @@ import { BufferGeometry, CylinderGeometry, SphereGeometry } from "three";
 import { mergeGeometries } from "three/addons/utils/BufferGeometryUtils.js";
 
 class TestTubeGeometry extends BufferGeometry {
-  constructor() {
+  constructor(radiusTop = 0.2, radiusBottom = 0.2, height = 3, segments = 32) {
     super();
 
     // Create the cylindrical body
-    const tubeGeometry = new CylinderGeometry(0.2, 0.2, 3, 32, 1, true); // Radius top, radius bottom, height
+    const tubeGeometry = new CylinderGeometry(radiusTop, radiusBottom, height, segments, 1, true); // Radius top, radius bottom, height
 
     // Create the rounded bottom using a sphere
-    const bottomGeometry = new SphereGeometry(0.2, 32, 16, 0, Math.PI * 2, Math.PI / 2, Math.PI / 2); // Half sphere
-    bottomGeometry.translate(0, -1.5, 0); // Position it at the bottom of the cylinder
+    const bottomGeometry = new SphereGeometry(radiusBottom, segments, segments / 2, 0, Math.PI * 2, Math.PI / 2, Math.PI / 2); // Half sphere
+    bottomGeometry.translate(0, -(height / 2), 0); // Position it at the bottom of the cylinder
 
     // Merge parts
     this.copy(mergeGeometries([tubeGeometry, bottomGeometry], true));
