6b90d854b38dac27efc35cd541a5b958a61a8f8c
Jason Sturges <jason@jasonsturges.com>
2024-10-31 00:07:31 -0500

    Test Tube geometry
    Open ended

--- src/geometry/science/TestTubeGeometry.js
+++ src/geometry/science/TestTubeGeometry.js
@@ -2,11 +2,11 @@ import { BufferGeometry, CylinderGeometry, SphereGeometry } from "three";
 import { mergeGeometries } from "three/addons/utils/BufferGeometryUtils.js";
 
 class TestTubeGeometry extends BufferGeometry {
-  constructor(radiusTop = 0.2, radiusBottom = 0.2, height = 3, segments = 32) {
+  constructor(radiusTop = 0.2, radiusBottom = 0.2, height = 3, segments = 32, openEnded = true) {
     super();
 
     // Create the cylindrical body
-    const tubeGeometry = new CylinderGeometry(radiusTop, radiusBottom, height, segments, 1, true);
+    const tubeGeometry = new CylinderGeometry(radiusTop, radiusBottom, height, segments, 1, openEnded);
 
     // Create the rounded bottom using a half sphere
     const bottomGeometry = new SphereGeometry(radiusBottom, segments, segments / 2, 0, Math.PI * 2, Math.PI / 2, Math.PI / 2);
