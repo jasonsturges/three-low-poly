e26fc94b73041ea866a32ef6b54bb40afb998386
Jason Sturges <jason@jasonsturges.com>
2024-10-31 00:30:07 -0500

    Beaker

--- src/group/science/Beaker.js
+++ src/group/science/Beaker.js
@@ -1,16 +1,12 @@
-import { CylinderGeometry, DoubleSide, Group, Mesh, MeshPhysicalMaterial, SphereGeometry } from "three";
+import { BeakerGeometry } from "../../geometry/science/BeakerGeometry.js";
+import { DoubleSide, Group, Mesh, MeshPhysicalMaterial } from "three";
 
 class Beaker extends Group {
   constructor() {
     super();
 
-    // Create the sphere part of the glassware
-    const sphereGeometry = new SphereGeometry(1, 16, 16);
-    const tubeGeometry = new CylinderGeometry(0.2, 0.2, 2, 16, 1, true);
-
-    // Adjust tube position to stick out of the sphere
-    tubeGeometry.translate(0, 1.5, 0);
-    tubeGeometry.rotateX(Math.PI / 2);
+    // Create the geometry
+    const beakerGeometry = new BeakerGeometry();
 
     // Create a group to combine the geometries
     const glassMaterial = new MeshPhysicalMaterial({
@@ -20,16 +16,13 @@ class Beaker extends Group {
       roughness: 0.1,
       metalness: 0.1,
       reflectivity: 0.8,
-      transmission: 0.9, // For realistic glass effect
+      transmission: 0.9,
       side: DoubleSide,
     });
 
-    const sphere = new Mesh(sphereGeometry, glassMaterial);
-    const tube = new Mesh(tubeGeometry, glassMaterial);
-
-    this.add(sphere);
-    this.add(tube);
-    this.rotateX(-Math.PI / 2);
+    const beaker = new Mesh(beakerGeometry, glassMaterial);
+    beaker.rotation.x = -Math.PI / 2;
+    this.add(beaker);
   }
 }
 
