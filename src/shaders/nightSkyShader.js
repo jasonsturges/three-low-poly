896d78764d7290a8db57daef06d3273d166031e1
Jason Sturges <jason@jasonsturges.com>
2024-11-02 17:18:24 -0500

    Night sky shader

---
+++ src/shaders/nightSkyShader.js
@@ -0,0 +1,29 @@
+import { Color } from "three";
+
+export const nightSkyShader = {
+  uniforms: {
+    topColor: { value: new Color(0x000033) },
+    bottomColor: { value: new Color(0x000011) },
+    offset: { value: 33 },
+    exponent: { value: 0.6 },
+  },
+  vertexShader: `
+    varying vec3 vWorldPosition;
+    void main() {
+      vec4 worldPosition = modelMatrix * vec4(position, 1.0);
+      vWorldPosition = worldPosition.xyz;
+      gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+    }
+  `,
+  fragmentShader: `
+    uniform vec3 topColor;
+    uniform vec3 bottomColor;
+    uniform float offset;
+    uniform float exponent;
+    varying vec3 vWorldPosition;
+    void main() {
+      float h = normalize(vWorldPosition + offset).y;
+      gl_FragColor = vec4(mix(bottomColor, topColor, max(pow(max(h, 0.0), exponent), 0.0)), 1.0);
+    }
+  `,
+};
