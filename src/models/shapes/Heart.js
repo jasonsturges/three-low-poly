77b2c8180f1f7e10f235df9c6619266c94888cb8
Jason Sturges <jason@jasonsturges.com>
2024-11-02 14:22:24 -0500

    Heart
    Material update

--- src/models/shapes/Heart.js
+++ src/models/shapes/Heart.js
@@ -12,7 +12,14 @@ class Heart extends Mesh {
       bevelThickness: 0,
       bevelSize: 0,
     });
-    const material = new MeshStandardMaterial({ color: 0xc62828 });
+    const material = new MeshStandardMaterial({
+      color: 0xc62828,
+      emissive: 0xc61416,
+      emissiveIntensity: 0.25,
+      metalness: 0.1,
+      roughness: 0.3,
+      flatShading: true,
+    });
 
     geometry.center();
     this.geometry = geometry;
