c12c6d7f0a73e89210537eed70d5aa694bef1eab
Jason Sturges <jason@jasonsturges.com>
2024-11-02 02:15:44 -0500

    Heart

---
+++ src/models/shapes/Heart.js
@@ -0,0 +1,23 @@
+import { ExtrudeGeometry, Mesh, MeshStandardMaterial } from "three";
+import { HeartShape } from "../../shapes/HeartShape.js";
+
+class Heart extends Mesh {
+  constructor(size = 1, width = 1, height = 1, tipDepth = 10, depth = 0.25) {
+    super();
+
+    const shape = new HeartShape(size, width, height, tipDepth);
+    const geometry = new ExtrudeGeometry(shape, {
+      depth: depth,
+      bevelEnabled: depth > 0,
+      bevelThickness: 0,
+      bevelSize: 0,
+    });
+    const material = new MeshStandardMaterial({ color: 0xc62828 });
+
+    geometry.center();
+    this.geometry = geometry;
+    this.material = material;
+  }
+}
+
+export { Heart };
