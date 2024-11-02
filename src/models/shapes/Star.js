7e3d8e5edd553abc7ff7f8dd716d84ee5ce18632
Jason Sturges <jason@jasonsturges.com>
2024-11-02 13:51:23 -0500

    Star
    Updated material

--- src/models/shapes/Star.js
+++ src/models/shapes/Star.js
@@ -12,7 +12,14 @@ class Star extends Mesh {
       bevelThickness: 0,
       bevelSize: 0,
     });
-    const material = new MeshStandardMaterial({ color: 0x2194ce });
+    const material = new MeshStandardMaterial({
+      color: 0xffff00,
+      emissive: 0xffd700,
+      emissiveIntensity: 0.25,
+      metalness: 0.1,
+      roughness: 0.3,
+      flatShading: true,
+    });
 
     geometry.center();
     this.geometry = geometry;
