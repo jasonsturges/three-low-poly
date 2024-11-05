8bd8c310d7e6ef86926d19bcd62a62e1319cdf60
Jason Sturges <jason@jasonsturges.com>
2024-11-04 21:34:07 -0600

    Rounded headstone

---
+++ src/models/cemetery/RoundedHeadstone.js
@@ -0,0 +1,11 @@
+import { Mesh, MeshStandardMaterial } from "three";
+import { RoundedHeadstoneGeometry } from "../../geometry/cemetery/RoundedHeadstoneGeometry.js";
+
+export class RoundedHeadstone extends Mesh {
+  constructor(width = 0.6, height = 1.0, depth = 0.2, radius = 0.6) {
+    super();
+
+    this.geometry = new RoundedHeadstoneGeometry(width, height, depth, radius);
+    this.material = new MeshStandardMaterial({ color: 0x777777, roughness: 0.8 });
+  }
+}
