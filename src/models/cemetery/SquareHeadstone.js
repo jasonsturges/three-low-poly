214c4c4be29f33d5dc69b91e338ecb9192f23dff
Jason Sturges <jason@jasonsturges.com>
2024-11-04 21:38:09 -0600

    Square headstone

---
+++ src/models/cemetery/SquareHeadstone.js
@@ -0,0 +1,11 @@
+import { Mesh, MeshStandardMaterial } from "three";
+import { SquareHeadstoneGeometry } from "../../geometry/cemetery/SquareHeadstoneGeometry.js";
+
+export class SquareHeadstone extends Mesh {
+  constructor(width = 0.5, height = 0.8, depth = 0.15) {
+    super();
+
+    this.geometry = new SquareHeadstoneGeometry(width, height, depth);
+    this.material = new MeshStandardMaterial({ color: 0x777777, roughness: 0.8 });
+  }
+}
