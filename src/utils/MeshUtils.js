0adb8ba64154a35a12da984be78a73d8ccb1722d
Jason Sturges <jason@jasonsturges.com>
2024-11-06 01:40:05 -0600

    Mesh utils

---
+++ src/utils/MeshUtils.js
@@ -0,0 +1,28 @@
+import { Box3, Vector3 } from "three";
+
+/**
+ * Centers the given mesh at the origin (0, 0, 0) of the scene by adjusting its position.
+ * The mesh is moved so that its bounding box center is placed at the origin.
+ */
+export function centerMesh(mesh) {
+  const box = new Box3().setFromObject(mesh);
+  const center = box.getCenter(new Vector3());
+
+  mesh.translateX(-center.x);
+  mesh.translateY(-center.y);
+  mesh.translateZ(-center.z);
+  mesh.updateMatrixWorld(true);
+}
+
+/**
+ * Centers the geometry of the given mesh at the local origin (0, 0, 0) by modifying the geometry vertices.
+ * This method moves the geometry itself so that its center is at the local origin,
+ * without affecting the position, rotation, or scale of the mesh.
+ */
+export function centerMeshGeometry(mesh) {
+  mesh.geometry.computeBoundingBox();
+  const box = mesh.geometry.boundingBox;
+  const center = box.getCenter(new Vector3());
+
+  mesh.geometry.translate(-center.x, -center.y, -center.z);
+}
