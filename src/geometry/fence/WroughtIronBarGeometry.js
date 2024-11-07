986504820e3e7f9665182c11acb24d46d60130cd
Jason Sturges <jason@jasonsturges.com>
2024-11-06 22:33:00 -0600

    Wrought iron bar geometry

--- src/geometry/fence/WroughtIronBarGeometry.js
+++ src/geometry/fence/WroughtIronBarGeometry.js
@@ -3,30 +3,26 @@ import { mergeGeometries } from "three/addons/utils/BufferGeometryUtils.js";
 
 /**
  * Wrought Iron Bar Geometry, a simple wrought iron bar shape
- * @extends BufferGeometry
- *
- * @example
- * // Create a wrought iron bar
- * const barGeometry = new WroughtIronBarGeometry();
- * const barMaterial = new MeshStandardMaterial({ color: 0x333333, metalness: 0.8, roughness: 0.4 });
- * const bar = new Mesh(barGeometry, barMaterial);
- * scene.add(bar);
  */
-class WroughtIronBarGeometry extends BufferGeometry {
-  constructor(barHeight = 2.0, barRadius = 0.05, spikeHeight = 0.3) {
+export class WroughtIronBarGeometry extends BufferGeometry {
+  constructor({
+    barHeight = 2.0, //
+    barRadius = 0.05,
+    spikeHeight = 0.3,
+    spikeRadius = 0.075, // barRadius * 1.5,
+    radialSegments = 8,
+  } = {}) {
     super();
 
     // Create a cylinder for the vertical bar
-    const barGeometry = new CylinderGeometry(barRadius, barRadius, barHeight, 8);
-    barGeometry.translate(0, barHeight / 2, 0); // Shift up to stand on the ground
+    const barGeometry = new CylinderGeometry(barRadius, barRadius, barHeight, radialSegments);
+    barGeometry.translate(0, barHeight / 2, 0);
 
     // Create a cone for the spike on top
-    const spikeGeometry = new ConeGeometry(barRadius * 1.5, spikeHeight, 8);
-    spikeGeometry.translate(0, barHeight + spikeHeight / 2, 0); // Place on top of the bar
+    const spikeGeometry = new ConeGeometry(spikeRadius, spikeHeight, radialSegments);
+    spikeGeometry.translate(0, barHeight + spikeHeight / 2, 0);
 
     // Merge bar and spike into one geometry
     this.copy(mergeGeometries([barGeometry, spikeGeometry], false));
   }
 }
-
-export { WroughtIronBarGeometry };
