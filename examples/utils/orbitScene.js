7aa2a292ffdfb94e0e0ee5fb9f74a91b02ce18b8
Jason Sturges <jason@jasonsturges.com>
2024-11-12 23:31:20 -0600

    Antialias renderer

--- examples/utils/orbitScene.js
+++ examples/utils/orbitScene.js
@@ -7,7 +7,7 @@ export function createOrbitScene() {
   const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
   camera.position.z = 5;
 
-  const renderer = new THREE.WebGLRenderer();
+  const renderer = new THREE.WebGLRenderer({ antialias: true });
   renderer.setClearColor(0x000000);
   renderer.setSize(window.innerWidth, window.innerHeight);
   renderer.shadowMap.enabled = true;
