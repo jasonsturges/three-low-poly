49455ac48cb8daa7dfcad34bfae33b86053cb00b
Jason Sturges <jason@jasonsturges.com>
2024-11-15 22:38:57 -0600

    Day skybox

--- src/skybox/DaySkybox.ts
+++ src/skybox/DaySkybox.ts
@@ -1,17 +1,21 @@
 import { BackSide, BoxGeometry, Mesh, ShaderMaterial } from "three";
-import { daySkyShader } from "../shaders/daySkyShader";
+import { daySkyShader, DaySkyUniforms } from "../shaders/daySkyShader";
 
 class DaySkybox extends Mesh {
+  geometry: BoxGeometry;
+  material: ShaderMaterial & { uniforms: DaySkyUniforms };
+
   constructor(size = 1000) {
     super();
 
     this.geometry = new BoxGeometry(size, size, size);
 
     this.material = new ShaderMaterial({
+      uniforms: daySkyShader.uniforms,
       vertexShader: daySkyShader.vertexShader,
       fragmentShader: daySkyShader.fragmentShader,
       side: BackSide,
-    });
+    }) as ShaderMaterial & { uniforms: DaySkyUniforms };
   }
 }
 
