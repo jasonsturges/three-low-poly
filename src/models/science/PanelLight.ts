03bbcab4c6d57b22fc23abe4fe2a9ade7e03e591
Jason Sturges <jason@jasonsturges.com>
2024-11-17 23:48:12 -0600

    Panel light

--- src/models/science/PanelLight.ts
+++ src/models/science/PanelLight.ts
@@ -1,6 +1,7 @@
 import { Mesh, MeshStandardMaterial, SphereGeometry } from "three";
 
 export interface PanelLightOptions {
+  radius?: number;
   color?: number;
   emissive?: number;
   emissiveIntensity?: number;
@@ -14,10 +15,15 @@ export class PanelLight extends Mesh {
   public geometry: SphereGeometry;
   public material: MeshStandardMaterial;
 
-  constructor({ color = 0xffc7c7, emissive = 0xff0000, emissiveIntensity = 0.5 }: PanelLightOptions = {}) {
+  constructor({
+    radius = 0.15, //
+    color = 0xffc7c7,
+    emissive = 0xff0000,
+    emissiveIntensity = 0.5,
+  }: PanelLightOptions = {}) {
     super();
 
-    this.geometry = new SphereGeometry(0.15, 8, 8);
+    this.geometry = new SphereGeometry(radius, 8, 8);
     this.material = new MeshStandardMaterial({
       color: color,
       emissive: emissive,
