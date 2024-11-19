7951164e4de3d85251d2cb704e0c6a44800fa10f
Jason Sturges <jason@jasonsturges.com>
2024-11-19 01:29:04 -0600

    Axis
    Documentation

--- src/constants/Axis.ts
+++ src/constants/Axis.ts
@@ -5,10 +5,106 @@ import { Vector3 } from "three";
  *
  * Example usages:
  *
+ * Defining a geometry's up vector:
+ * ```
+ * const upVector = Axis.Z; // Set the Z-axis as the "up" direction
+ * object.up.copy(upVector);
+ * ```
+ *
  * Rotating or Orienting an Object
  * ```
  * const axis = Axis.XY; // Diagonal upward
  * object.lookAt(object.position.clone().add(axis));
+ * ```
+ *
+ * Rotate around axis:
+ * ```
+ * const angle = Math.PI / 4; // 45-degree rotation
+ * const rotationAxis = Axis.Y; // Rotate around the Y-axis
+ *
+ * object.rotateOnAxis(rotationAxis, angle);
+ * ```
+ *
+ * Scaling along an axis:
+ * ```
+ * const scalingAxis = Axis.XZ; // Scale equally along X and Z
+ * const scaleFactor = 2;
+ *
+ * object.scale.multiply(scalingAxis.clone().multiplyScalar(scaleFactor));
+ * ```
+ *
+ * Aligning a Camera
+ * ```
+ * const cameraAxis = Axis.XZ; // Align the camera diagonally on the XZ plane
+ *
+ * camera.lookAt(camera.position.clone().add(cameraAxis));
+ * ```
+ *
+ * Directional lighting
+ * ```
+ * const lightDirection = Axis.YZ; // Diagonal light along Y and Z axes
+ * directionalLight.position.copy(lightDirection.clone().multiplyScalar(10));
+ * ```
+ *
+ * Aligning an Object
+ * ```
+ * const axis = Axis.X; // Align object to the X-axis
+ * object.lookAt(object.position.clone().add(axis));
+ * ```
+ *
+ * Spawning objects along an axis
+ * ```
+ * const spawnAxis = Axis.XZ; // Arrange objects diagonally on XZ
+ * const count = 10;
+ * const spacing = 5;
+ *
+ * for (let i = 0; i < count; i++) {
+ *   const position = spawnAxis.clone().multiplyScalar(i * spacing);
+ *   const newObject = object.clone();
+ *   newObject.position.copy(position);
+ *   scene.add(newObject);
+ * }
+ * ```
+ *
+ * Vector projections
+ * ```
+ * const vector = new Vector3(3, 5, 7);
+ * const projectionAxis = Axis.Z; // Project the vector onto the Z-axis
+ *
+ * const projection = vector.clone().projectOnVector(projectionAxis);
+ * ```
+ *
+ * Plane definitions
+ * ```
+ * const normal = Axis.Y; // Y-axis is the normal for a horizontal plane
+ * const distanceFromOrigin = 5;
+ *
+ * const plane = new THREE.Plane(normal, distanceFromOrigin);
+ * ```
+ *
+ * Plane intersections
+ * ```
+ * const planeNormal = Axis.Y; // Define a plane normal (horizontal plane)
+ * const plane = new THREE.Plane(planeNormal);
+ *
+ * const rayDirection = Axis.Z; // Ray pointing along Z-axis
+ * const rayOrigin = new Vector3(0, 5, 0);
+ * const ray = new THREE.Ray(rayOrigin, rayDirection);
+ *
+ * // Find intersection point
+ * const intersectionPoint = new Vector3();
+ * plane.intersectLine(new THREE.Line3(rayOrigin, rayOrigin.clone().add(rayDirection)), intersectionPoint);
+ * ```
+ *
+ * Defining bounds
+ * ```
+ * const boundsAxis = Axis.XY; // Restrict an object’s movement within XY bounds
+ * const maxBounds = 10;
+ *
+ * object.position.clamp(
+ *   new Vector3(-maxBounds, -maxBounds, -Infinity),
+ *   new Vector3(maxBounds, maxBounds, Infinity)
+ * );
  * ```
  *
  * Procedural Geometry
@@ -20,7 +116,6 @@ import { Vector3 } from "three";
  * vertex.add(offset); // Move vertex in axis
  * geometry.vertices.push(vertex);
  * ```
- *
  */
 export const Axis = {
   X: new Vector3(1, 0, 0),
