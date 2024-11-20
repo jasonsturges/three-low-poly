921310e2f1211740792a1b7506efe2cb4f1cf918
Jason Sturges <jason@jasonsturges.com>
2024-11-20 01:47:25 -0600

    Book

--- src/models/books/Book.ts
+++ src/models/books/Book.ts
@@ -1,30 +1,28 @@
 import { Mesh, MeshStandardMaterial } from "three";
 import { BookGeometry } from "../../geometry/books/BookGeometry";
+import { ColorPalette } from "../../constants/ColorPalette";
 
 /**
  * Material Order:
  *   1. Cover
  *   2. Page
  */
-export class Book extends Mesh {
-  public geometry: BookGeometry;
-  public material: MeshStandardMaterial[];
-
+export class Book extends Mesh<BookGeometry, MeshStandardMaterial[]> {
   constructor({
     width = 1,
     height = 1.5,
     depth = 0.5,
     coverThickness = 0.05,
     pageIndent = 0.05,
-    coverColor = 0x8b0000,
-    pageColor = 0xffffff,
+    coverColor = ColorPalette.DARK_RED,
+    pageColor = ColorPalette.WHITE,
   } = {}) {
-    super();
-
-    this.geometry = new BookGeometry(width, height, depth, coverThickness, pageIndent);
-    this.material = [
-      new MeshStandardMaterial({ color: coverColor, metalness: 0.1, roughness: 0.7, flatShading: true }),
-      new MeshStandardMaterial({ color: pageColor, flatShading: true }),
-    ];
+    super(
+      new BookGeometry(width, height, depth, coverThickness, pageIndent),
+      [
+        new MeshStandardMaterial({ color: coverColor, metalness: 0.1, roughness: 0.7, flatShading: true }),
+        new MeshStandardMaterial({ color: pageColor, flatShading: true }),
+      ],
+    );
   }
 }
