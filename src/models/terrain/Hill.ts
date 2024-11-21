4074345ca95aeadfce4e9bdd4f8fceb8415efc9b
Jason Sturges <jason@jasonsturges.com>
2024-11-21 01:03:30 -0600

    Hill
    Prefab

--- src/models/terrain/Hill.ts
+++ src/models/terrain/Hill.ts
@@ -1,7 +1,7 @@
 import { Mesh, MeshStandardMaterial } from "three";
 import { HillGeometry } from "../../geometry/terrain/HillGeometry";
 
-export class Hill extends Mesh {
+export class Hill extends Mesh<HillGeometry, MeshStandardMaterial> {
   constructor({
     radius = 3, //
     height = 0.6,
@@ -10,17 +10,16 @@ export class Hill extends Mesh {
     phiStart = 0,
     phiLength = Math.PI * 2,
   } = {}) {
-    super();
-
-    this.geometry = new HillGeometry({
-      radius,
-      height,
-      widthSegments,
-      heightSegments,
-      phiStart,
-      phiLength,
-    });
-
-    this.material = new MeshStandardMaterial({ color: 0x00ff00, flatShading: true });
+    super(
+      new HillGeometry({
+        radius,
+        height,
+        widthSegments,
+        heightSegments,
+        phiStart,
+        phiLength,
+      }),
+      new MeshStandardMaterial({ color: 0x00ff00, flatShading: true }),
+    );
   }
 }
