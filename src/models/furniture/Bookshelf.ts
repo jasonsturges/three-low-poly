173765418beae6a7b292d2fa7fff973fdb8ea31b
Jason Sturges <jason@jasonsturges.com>
2024-11-20 21:06:33 -0600

    Bookshelf

--- src/models/furniture/Bookshelf.ts
+++ src/models/furniture/Bookshelf.ts
@@ -1,7 +1,7 @@
 import { Mesh, MeshStandardMaterial } from "three";
 import { BookshelfGeometry } from "../../geometry/furniture/BookshelfGeometry";
 
-export class Bookshelf extends Mesh {
+export class Bookshelf extends Mesh<BookshelfGeometry, MeshStandardMaterial> {
   constructor({
     width = 5, //
     height = 8,
@@ -10,9 +10,9 @@ export class Bookshelf extends Mesh {
     frameThickness = 0.1,
     open = false,
   } = {}) {
-    super();
-
-    this.geometry = new BookshelfGeometry({ width, height, depth, shelves, frameThickness, open });
-    this.material = new MeshStandardMaterial({ color: 0x8b4513 });
+    super(
+      new BookshelfGeometry({ width, height, depth, shelves, frameThickness, open }),
+      new MeshStandardMaterial({ color: 0x8b4513 }),
+    );
   }
 }
