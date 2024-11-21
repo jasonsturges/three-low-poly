fee7135b567288a9ff5c35348f1add92f0c13df0
Jason Sturges <jason@jasonsturges.com>
2024-11-20 21:07:43 -0600

    Rock

--- src/models/rocks/Rock.ts
+++ src/models/rocks/Rock.ts
@@ -1,12 +1,11 @@
 import { Mesh, MeshStandardMaterial } from "three";
 import { RockGeometry } from "../../geometry/rocks/RockGeometry";
 
-export class Rock extends Mesh {
+export class Rock extends Mesh<RockGeometry, MeshStandardMaterial> {
   constructor(radius = 1, widthSegments = 4, heightSegments = 4) {
-    super();
-
-    // Create a rock geometry
-    this.geometry = new RockGeometry(radius, widthSegments, heightSegments);
-    this.material = new MeshStandardMaterial({ color: 0x808080, flatShading: true });
+    super(
+      new RockGeometry(radius, widthSegments, heightSegments),
+      new MeshStandardMaterial({ color: 0x808080, flatShading: true }),
+    );
   }
 }
