54c35dbf5b4dafc014bc85bcfe49b5b6fd2e09b3
Jason Sturges <jason@jasonsturges.com>
2024-11-20 20:29:04 -0600

    Color palette

--- src/constants/ColorPalette.ts
+++ src/constants/ColorPalette.ts
@@ -1,68 +1,84 @@
 export const ColorPalette = {
   // Red
-  CARDINAL_RED: 0xc61416,  // Bright Crimson or Cardinal Red. It’s a vivid, bold red shade with warm undertones.
-  CHERRY_RED: 0xd82424,    // Bold red with slightly warm undertones.
-  CRIMSON: 0xc62828,       // Crimson Red or Firebrick Red. It’s a bold, deep red with a slightly cool undertone.
-  RUST: 0xc2452d,          // Red clay or Brick Red. It’s a warm, earthy red-orange shade, often associated with weathered metal, autumn leaves, or natural clay tones.
-  DARK_RED: 0x8b0000,      // Dark Red. It’s a deep, rich red shade resembling the color of garnets or dried blood.
+  CADMIUM_RED: 0xe60026,      // Cadmium Red or Bright Red. Vivid, bold red shade with warm undertones.
+  CARDINAL_RED: 0xc61416,     // Bright Crimson or Cardinal Red. It’s a vivid, bold red shade with warm undertones.
+  CHERRY_RED: 0xd82424,       // Bold red with slightly warm undertones.
+  CRIMSON: 0xc62828,          // Crimson Red or Firebrick Red. It’s a bold, deep red with a slightly cool undertone.
+  ALIZARIN_CRIMSON: 0xb22222, // Deep, rich red with cool undertones.
+  RUST: 0xc2452d,             // Red clay or Brick Red. It’s a warm, earthy red-orange shade, often associated with weathered metal, autumn leaves, or natural clay tones.
+  DARK_RED: 0x8b0000,         // Dark Red. It’s a deep, rich red shade resembling the color of garnets or dried blood.
 
   // Orange
-  TANGERINE: 0xff5500,     // Tangerine or Bright Orange-Red. It’s a vibrant, warm shade of orange with red undertones with a  tropical feel.
-  ORANGE_PEEL: 0xff6600,   // Orange Peel or Bright Orange. It’s a vivid, energetic orange.
-  ORANGE: 0xffa500,        // Bright, vibrant hue associated with sunsets, citrus fruits, and autumn tones.
+  TANGERINE: 0xff5500,        // Tangerine or Bright Orange-Red. It’s a vibrant, warm shade of orange with red undertones with a  tropical feel.
+  ORANGE_PEEL: 0xff6600,      // Orange Peel or Bright Orange. It’s a vivid, energetic orange.
+  CADMIUM_ORANGE: 0xff7f24,   // warm orange with strong red undertones, giving it a slightly richer and darker appearance.
+  ORANGE: 0xffa500,           // Bright, vibrant hue associated with sunsets, citrus fruits, and autumn tones.
 
   // Yellow
-  AMBER: 0xffaa00,         // Bright Orange. It’s a warm, vibrant shade of orange with golden-yellow undertones, associated with autumn hues.
-  GOLD: 0xffd700,          // Rich, vibrant yellow with warm undertones, resembling the color of metallic gold.
-  YELLOW: 0xffff00,        // Bright, pure yellow shade.
+  AMBER: 0xffaa00,            // Bright Orange. It’s a warm, vibrant shade of orange with golden-yellow undertones, associated with autumn hues.
+  CADMIUM_YELLOW: 0xffd700,   // Bright golden yellow with warm undertones.
+  GOLD: 0xffd700,             // Rich, vibrant yellow with warm undertones, resembling the color of metallic gold.
+  YELLOW: 0xffff00,           // Bright, pure yellow shade.
 
   // Green
-  LIME_GREEN: 0xa4c400,    // Lime Green or Bright Chartreuse. It’s a vivid, energetic yellow-green shade
-  SPRING_GREEN: 0x88cc55,  // Fresh, vibrant appearance, resembling new leaves or spring foliage
-  MOSS_GREEN: 0x88aa33,    // Medium-light shade of green with yellow undertones, often seen in natural foliage.
-  FERN_GREEN: 0x4b8b3b,    // It's a natural, medium-dark green with slightly muted tones, resembling the lush greenery of ferns or foliage.
-  FOREST_GREEN: 0x228b22,  // Rich, deep green shade, often associated with dense woodland and nature.
-  OLIVE_DRAB: 0x556b2f,    // Muted earthy green with brownish undertones.
-  MINT_GREEN: 0x88ccaa,    // Mint Green or Light Jade. It’s a soft, pastel green with subtle blue undertones.
-  AQUAMARINE: 0x00ffaa,    // Aquamarine or Mint Green. It’s a bright, vibrant turquoise shade that blends green and blue.
+  LIME_GREEN: 0xa4c400,       // Lime Green or Bright Chartreuse. It’s a vivid, energetic yellow-green shade
+  SPRING_GREEN: 0x88cc55,     // Fresh, vibrant appearance, resembling new leaves or spring foliage
+  MOSS_GREEN: 0x88aa33,       // Medium-light shade of green with yellow undertones, often seen in natural foliage.
+  FERN_GREEN: 0x4b8b3b,       // It's a natural, medium-dark green with slightly muted tones, resembling the lush greenery of ferns or foliage.
+  FOREST_GREEN: 0x228b22,     // Rich, deep green shade, often associated with dense woodland and nature.
+  SAP_GREEN: 0x507d2a,        // Natural green with a muted tone.
+  OLIVE_DRAB: 0x556b2f,       // Muted earthy green with brownish undertones.
+  VIRIDIAN_GREEN: 0x00a86b,   // Vibrant cool green.
+  MINT_GREEN: 0x88ccaa,       // Mint Green or Light Jade. It’s a soft, pastel green with subtle blue undertones.
+  AQUAMARINE: 0x00ffaa,       // Aquamarine or Mint Green. It’s a bright, vibrant turquoise shade that blends green and blue.
 
   // Blue
-  SKY_BLUE: 0x88ccff,      // Sky Blue or Baby Blue. It’s a light, cheerful shade of blue with a slightly soft and pastel tone, evoking a sense of openness like a clear daytime sky.
-  CERULEAN_BLUE: 0x5dade2, // Cerulean Blue or Sky Blue. It’s a bright, vivid shade of blue with a slightly cool undertone, often associated with clear skies or tropical waters.
-  AZURE: 0x0077ff,         // Azure Blue or Bright Blue. It’s a vibrant, rich shade of blue with a cool undertone, often associated with clear skies or digital interfaces.
-  OCEAN_BLUE: 0x0077be,    // Ocean Blue or Azure Blue. It’s a vibrant medium blue shade with a slight teal undertone, often associated with clear skies or tropical waters.
-  ROYAL_BLUE: 0x5555ff,    // Royal Blue or Bright Indigo. It’s a vibrant medium blue shade with a slightly cool and saturated tone.
-  MIDNIGHT_BLUE: 0x000033, // A very deep, dark blue shade that evokes the feeling of a starless night sky or deep ocean depths, with subtle blue undertones.
+  PHTHALO_BLUE: 0x000f89,     // Phthalo Blue or Bright Navy.  Intense, rich blue with a slightly greenish cool undertone.
+  SKY_BLUE: 0x88ccff,         // Sky Blue or Baby Blue. It’s a light, cheerful shade of blue with a slightly soft and pastel tone, evoking a sense of openness like a clear daytime sky.
+  CERULEAN_BLUE: 0x5dade2,    // Cerulean Blue or Sky Blue. It’s a bright, vivid shade of blue with a slightly cool undertone, often associated with clear skies or tropical waters.
+  AZURE: 0x0077ff,            // Azure Blue or Bright Blue. It’s a vibrant, rich shade of blue with a cool undertone, often associated with clear skies or digital interfaces.
+  OCEAN_BLUE: 0x0077be,       // Ocean Blue or Azure Blue. It’s a vibrant medium blue shade with a slight teal undertone, often associated with clear skies or tropical waters.
+  ROYAL_BLUE: 0x5555ff,       // Royal Blue or Bright Indigo. It’s a vibrant medium blue shade with a slightly cool and saturated tone.
+  MIDNIGHT_BLUE: 0x000033,    // A very deep, dark blue shade that evokes the feeling of a starless night sky or deep ocean depths, with subtle blue undertones.
+  ULTRAMARINE_BLUE: 0x3f51b5, // Deep, rich blue with a slightly cool undertone.
 
   // Purple
-  CORAL_PINK: 0xff3366,    // Bright Rose or Coral Pink. It’s a vivid, bold pinkish-red shade with warm undertones.
-  VIVID_MAGENTA: 0xff22ff, // It's an intense, electric shade of pink with equal parts red and blue.
-  MAGENTA: 0xff00ff,       // Bright, vivid hue that is a mix of equal parts red and blue, sometimes called Fuchsia.
-  HOT_PINK: 0xff00aa,      // Hot Pink or Fuchsia Rose. It’s a vivid and bold pink with a magenta undertone.
-  PINK_SHERBET: 0xff99cc,  // Pink Sherbet or Light Pink. It’s a soft, playful pink shade with a hint of warmth.
-  SOFT_PINK: 0xffc7c7,     // Blush Pink or Soft Pink. It’s a pale, delicate pink with warm undertones.
-  DEEP_VIOLET: 0xaa00ff,   // Electric Purple or Deep Violet. It’s a bright, vibrant shade of purple with a strong blue undertone.
+  COBALT_VIOLET: 0x6a0dad,    // Soft, purple tone.
+  DEEP_VIOLET: 0xaa00ff,      // Electric Purple or Deep Violet. It’s a bright, vibrant shade of purple with a strong blue undertone.
+  CORAL_PINK: 0xff3366,       // Bright Rose or Coral Pink. It’s a vivid, bold pinkish-red shade with warm undertones.
+  VIVID_MAGENTA: 0xff22ff,    // It's an intense, electric shade of pink with equal parts red and blue.
+  MAGENTA: 0xff00ff,          // Bright, vivid hue that is a mix of equal parts red and blue, sometimes called Fuchsia.
+  HOT_PINK: 0xff00aa,         // Hot Pink or Fuchsia Rose. It’s a vivid and bold pink with a magenta undertone.
+  PINK_SHERBET: 0xff99cc,     // Pink Sherbet or Light Pink. It’s a soft, playful pink shade with a hint of warmth.
+  SOFT_PINK: 0xffc7c7,        // Blush Pink or Soft Pink. It’s a pale, delicate pink with warm undertones.
 
   // Brown
-  TAUPE: 0x8b7d7b,         // Rosy Brown or Taupe Gray. It’s a muted, earthy tone with a mix of brown and gray.
-  SIENNA: 0x8b5a2b,        // Dark Goldenrod Brown. It’s a warm, earthy brown with reddish and golden undertones, often associated with natural clay or autumn leaves.
-  SADDLE_BROWN: 0x8b4513,  // Rich, earthy brown with reddish undertones, often associated with leather, wood, or other natural materials.
-  COFFEE_BROWN: 0x5c4033,  // Rich, earthy tone with hints of red and gray, resembling the color of natural wood or dark leather.
-  DARK_UMBER: 0x4b3621,    // Coffee Brown or Dark Umber. It’s a deep, rich brown with warm, earthy undertones, often resembling dark wood or roasted coffee beans.
+  BURNT_SIENNA: 0xe97451,     // warm, earthy brown with reddish undertones.
+  BURNT_UMBER: 0x8a3324,      // Dark brown with red undertones.
+  SIENNA: 0x8b5a2b,           // Dark Goldenrod Brown. It’s a warm, earthy brown with reddish and golden undertones, often associated with natural clay or autumn leaves.
+  SADDLE_BROWN: 0x8b4513,     // Rich, earthy brown with reddish undertones, often associated with leather, wood, or other natural materials.
+  COFFEE_BROWN: 0x5c4033,     // Rich, earthy tone with hints of red and gray, resembling the color of natural wood or dark leather.
+  DARK_UMBER: 0x4b3621,       // Coffee Brown or Dark Umber. It’s a deep, rich brown with warm, earthy undertones, often resembling dark wood or roasted coffee beans.
+  RAW_UMBER: 0x826644,        // Cool, muted brown.
+  YELLOW_OCHRE: 0xc5a16f,     // Warm, muted yellow-brown
+  RAW_SIENNA: 0xd2b48c,       // Light, golden brown.
+  TAUPE: 0x8b7d7b,            // Rosy Brown or Taupe Gray. It’s a muted, earthy tone with a mix of brown and gray.
 
   // Gray
-  ONYX: 0x030303,          // Extremely deep, almost black shade with a neutral tone.
-  CARBON: 0x2e2e2e,        // Deep, neutral gray.
-  CHARCOAL: 0x333333,      // Rich, neutral gray.
-  SLATE_GRAY: 0x404040,    // Medium-dark shade of gray
-  ASH_GRAY: 0x444444,      // Ash Gray or Dark Slate Gray.
-  STEEL_GRAY: 0x555555,    // Medium Gray or Steel Gray.
-  COOL_GRAY: 0x666666,     // It’s a medium-dark neutral gray.
-  IRON: 0x777777,          // It’s a medium gray neutral.
-  GRAY: 0x808080,          // It is a neutral, medium-gray tone that sits exactly halfway between black and white.
-  STONE: 0x888888,         // It’s a medium-light neutral.
-  SILVER: 0xaaaaaa,        // Light Gray. It’s a soft, pale gray shade with a slightly metallic undertone.
-  PALE_GRAY: 0xeeeeee,     // Very Light Gray or Soft Gray. It’s a pale, almost white shade of gray.
-  WHITE_SMOKE: 0xf5f5f5,   // White Smoke or Light Gray. It’s a soft, pale gray shade with a hint of warmth.
-  WHITE: 0xffffff,         // Pure white, the brightest color possible.
+  ONYX: 0x030303,             // Extremely deep, almost black shade with a neutral tone.
+  CARBON: 0x2e2e2e,           // Deep, neutral gray.
+  CHARCOAL: 0x333333,         // Rich, neutral gray.
+  SLATE_GRAY: 0x404040,       // Medium-dark shade of gray
+  ASH_GRAY: 0x444444,         // Ash Gray or Dark Slate Gray.
+  GRAPHITE: 0x505050,         // Medium-dark, neutral Graphite Gray or Smoky Gray shadowy muted depth.
+  STEEL_GRAY: 0x555555,       // Medium Gray or Steel Gray.
+  DIM_GRAY: 0x696969,         // Medium-dark neutral gray with a muted, balanced tone
+  IRON: 0x777777,             // It’s a medium gray neutral.
+  GRAY: 0x808080,             // It is a neutral, medium-gray tone that sits exactly halfway between black and white.
+  STONE: 0x888888,            // It’s a medium-light neutral.
+  SILVER: 0xaaaaaa,           // Light Gray. It’s a soft, pale gray shade with a slightly metallic undertone.
+  LIGHT_GRAY: 0xcccccc,       // Light, neutral Gray, sits between medium grays and white.
+  PALE_GRAY: 0xeeeeee,        // Very Light Gray or Soft Gray. It’s a pale, almost white shade of gray.
+  WHITE_SMOKE: 0xf5f5f5,      // White Smoke or Light Gray. It’s a soft, pale gray shade with a hint of warmth.
+  TITANIUM_WHITE: 0xffffff,   // Pure white, the brightest color possible.
  };
