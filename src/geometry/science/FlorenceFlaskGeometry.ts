ae42cd7335fda261a0ddacb7756e4088b988c31a
Jason Sturges <jason@jasonsturges.com>
2024-11-21 00:55:36 -0600

    Florence flask geometry

--- src/geometry/science/FlorenceFlaskGeometry.ts
+++ src/geometry/science/FlorenceFlaskGeometry.ts
@@ -1,19 +1,32 @@
-import { BufferGeometry, CylinderGeometry, SphereGeometry } from "three";
-import { mergeBufferGeometries } from "three-stdlib";
+import { BufferGeometry, LatheGeometry, Vector2 } from "three";
+import { thetaLengthForRadius } from "../../utils/SphericalGeometryUtils";
 
 export class FlorenceFlaskGeometry extends BufferGeometry {
   constructor() {
     super();
 
-    // Create the sphere part of the glassware
-    const sphereGeometry = new SphereGeometry(1, 16, 16);
-    const tubeGeometry = new CylinderGeometry(0.2, 0.2, 2, 16, 1, true);
+    // Parameters
+    const radius = 1; // Sphere radius
+    const numPoints = 32; // Number of points to define the semicircle
+    const numSegments = 32; // Number of segments for the lathe geometry
+    const holRadius = 0.2;
+    const thetaLength = thetaLengthForRadius(radius, holRadius);
 
-    // Adjust tube position to stick out of the sphere
-    tubeGeometry.translate(0, 1.5, 0);
-    tubeGeometry.rotateX(Math.PI / 2);
+    const points = [];
 
-    this.copy(mergeBufferGeometries([sphereGeometry, tubeGeometry], false) as BufferGeometry);
-    this.rotateX(-Math.PI / 2);
+    // Tube
+    points.push(new Vector2(0.2, 2.5));
+
+    // Generate points for the semicircle
+    for (let i = 0; i <= numPoints; i++) {
+      const theta = thetaLength + ((Math.PI - thetaLength) / numPoints) * i; // Start at thetaLength
+      const x = radius * Math.sin(theta); // x = r * sin(θ)
+      const y = radius * Math.cos(theta); // y = r * cos(θ)
+      points.push(new Vector2(x, y));
+    }
+
+    // Create LatheGeometry
+    const latheGeometry = new LatheGeometry(points, numSegments);
+    this.copy(latheGeometry);
   }
 }
