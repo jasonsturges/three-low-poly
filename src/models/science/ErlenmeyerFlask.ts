25e3894b11cda4fc2e3d5d42f5fa7d901fbec165
Jason Sturges <jason@jasonsturges.com>
2024-11-20 21:08:10 -0600

    Erlenmeyer Flask

--- src/models/science/ErlenmeyerFlask.ts
+++ src/models/science/ErlenmeyerFlask.ts
@@ -1,7 +1,7 @@
 import { ErlenmeyerFlaskGeometry } from "../../geometry/science/ErlenmeyerFlaskGeometry";
 import { DoubleSide, Mesh, MeshPhysicalMaterial } from "three";
 
-export class ErlenmeyerFlask extends Mesh {
+export class ErlenmeyerFlask extends Mesh<ErlenmeyerFlaskGeometry, MeshPhysicalMaterial> {
   constructor({
     flaskRadius = 1, //
     neckRadius = 0.3,
@@ -9,19 +9,19 @@ export class ErlenmeyerFlask extends Mesh {
     neckHeight = 1,
     radialSegments = 16,
   } = {}) {
-    super();
-
-    this.geometry = new ErlenmeyerFlaskGeometry({ flaskRadius, neckRadius, height, neckHeight, radialSegments });
-    this.material = new MeshPhysicalMaterial({
-      color: 0x88ccff,
-      transparent: true,
-      opacity: 0.4,
-      roughness: 0.1,
-      metalness: 0.1,
-      reflectivity: 0.8,
-      transmission: 0.9,
-      side: DoubleSide,
-      wireframe: false,
-    });
+    super(
+      new ErlenmeyerFlaskGeometry({ flaskRadius, neckRadius, height, neckHeight, radialSegments }),
+      new MeshPhysicalMaterial({
+        color: 0x88ccff,
+        transparent: true,
+        opacity: 0.4,
+        roughness: 0.1,
+        metalness: 0.1,
+        reflectivity: 0.8,
+        transmission: 0.9,
+        side: DoubleSide,
+        wireframe: false,
+      }),
+    );
   }
 }
