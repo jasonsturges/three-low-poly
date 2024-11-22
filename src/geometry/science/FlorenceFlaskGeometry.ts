516ef5f228fb45dd7fad1b1f7eed04be76dcd7c2
Jason Sturges <jason@jasonsturges.com>
2024-11-22 00:42:21 -0600

    Florence flask
    Spherical curve

--- src/geometry/science/FlorenceFlaskGeometry.ts
+++ src/geometry/science/FlorenceFlaskGeometry.ts
@@ -1,32 +1,17 @@
 import { BufferGeometry, LatheGeometry, Vector2 } from "three";
-import { thetaLengthForRadius } from "../../utils/SphericalGeometryUtils";
+import { appendSphericalCurve } from "../../utils/SphericalCurve";
 
 export class FlorenceFlaskGeometry extends BufferGeometry {
   constructor() {
     super();
 
-    // Parameters
-    const radius = 1; // Sphere radius
-    const numPoints = 32; // Number of points to define the semicircle
-    const numSegments = 32; // Number of segments for the lathe geometry
-    const holRadius = 0.2;
-    const thetaLength = thetaLengthForRadius(radius, holRadius);
-
-    const points = [];
-
-    // Tube
-    points.push(new Vector2(0.2, 2.5));
-
-    // Generate points for the semicircle
-    for (let i = 0; i <= numPoints; i++) {
-      const theta = thetaLength + ((Math.PI - thetaLength) / numPoints) * i; // Start at thetaLength
-      const x = radius * Math.sin(theta); // x = r * sin(θ)
-      const y = radius * Math.cos(theta); // y = r * cos(θ)
-      points.push(new Vector2(x, y));
-    }
+    const points = [
+      ...appendSphericalCurve(1, 1, 0.5, 0.2, 0, 32),
+      new Vector2(0.2, 3),
+    ];
 
     // Create LatheGeometry
-    const latheGeometry = new LatheGeometry(points, numSegments);
+    const latheGeometry = new LatheGeometry(points, 32);
     this.copy(latheGeometry);
   }
 }
