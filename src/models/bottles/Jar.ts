d31951fb8ea4093227ae6a538253626820a13765
Jason Sturges <jason@jasonsturges.com>
2024-11-21 22:04:09 -0600

    Jar
    Prefab

---
+++ src/models/bottles/Jar.ts
@@ -0,0 +1,25 @@
+import { Mesh, MeshStandardMaterial } from "three";
+import { JarGeometry } from "../../geometry/bottles/JarGeometry";
+
+/**
+ * Material indices
+ * 0. Jar
+ * 1. Cork
+ */
+export class Jar extends Mesh<JarGeometry, MeshStandardMaterial[]> {
+  constructor() {
+    super(new JarGeometry(), [
+      new MeshStandardMaterial({
+        color: 0x88ccaa,
+        transparent: true,
+        opacity: 0.4,
+        roughness: 0.1,
+        metalness: 0.5,
+      }),
+      new MeshStandardMaterial({
+        color: 0x8b4513,
+        roughness: 1.0,
+      }),
+    ]);
+  }
+}
