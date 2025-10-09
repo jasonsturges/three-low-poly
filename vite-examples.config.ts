906f5ede013753d12efcb8915ea019792cd73bc6
Jason Sturges <jason@jasonsturges.com>
2025-10-08 22:39:06 -0500

    Revert examples

--- vite-examples.config.ts
+++ vite-examples.config.ts
@@ -1,13 +1,36 @@
-import { defineConfig } from "vite";
-import { resolve } from "node:path";
+import fs from "fs";
+import { defineConfig, UserConfig } from "vite";
+import { resolve } from "path";
+
+function getAllHtmlFiles(path: string, files: string[] = []): string[] {
+  const folder = fs.readdirSync(path);
+
+  folder.forEach((file) => {
+    const filePath = resolve(path, file);
+    if (fs.statSync(filePath).isDirectory()) {
+      files = getAllHtmlFiles(filePath, files);
+    } else if (file.endsWith(".html")) {
+      files.push(filePath);
+    }
+  });
+
+  return files;
+}
 
 export default defineConfig({
   base: "/three-low-poly",
   root: "./examples",
   publicDir: "../public",
-  resolve: {
-    alias: {
-      "../../src/index.js": resolve(__dirname, "src/index.ts"),
+  plugins: [],
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+    rollupOptions: {
+      input: getAllHtmlFiles(resolve(__dirname, "examples")).reduce((entries: Record<string, string>, file: string) => {
+        const name = file.replace(resolve(__dirname, "examples") + "/", "").replace(".html", "");
+        entries[name] = file;
+        return entries;
+      }, {}),
     },
   },
-});
+} satisfies UserConfig);
